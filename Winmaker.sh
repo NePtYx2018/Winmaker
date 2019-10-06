af4c8fb6a943245f75cfcb9ccd2857eac2e4b0f7
NePtYx2018 <52055199+NePtYx2018@users.noreply.github.com>
2019-10-06 18:22:38 -0300

    Create Winmaker.sh

---
+++ Winmaker.sh
@@ -0,0 +1,30 @@
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Lo sentimos por hacerte esperar,pero Winmaker se"
+echo "esta descomprimiendo..."
+unzip Winmaker.zip
+mkdir $HOME/Status
+mv Winmaker $HOME/Status
+rm -fr $HOME/Winmaker
+mv $HOME/Status/Winmaker $HOME/
+rm -fr $HOME/Status
+clear
+echo ' _   _      ____  _ __   __'
+echo '| \ | | ___|  _ \| |\ \ / /_  __'
+echo '|  \| |/ _ \ |_) | __\ V /\ \/ /'
+echo '| |\  |  __/  __/| |_ | |  >  <'
+echo '|_| \_|\___|_|    \__||_| /_/\_\'
+echo '////////////////////////////////'
+echo '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\'
+echo ""
+echo "Gracias por descargar Winmaker pasate por"
+echo "mi canal ayi subo cosas interesantes:"
+echo "[https://youtu.be/UrOBPSU6u-8]"
+echo ""
